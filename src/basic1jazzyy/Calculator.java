8291db46b0da0a9c4025f33d0df55ae753ab24a5
javaclass <javaclass@15000548cpu.fec.org>
2017-09-26 15:25:06 -0500

    Please Use this Calculator

---
+++ src/basic1jazzyy/Calculator.java
@@ -0,0 +1,12 @@
+package basic1jazzyy;
+
+public class Calculator {
+	public static void main(String[] args) {
+		System.out.print(calculatesum(18, 2));
+}
+static int calculatesum(int a, int b) {
+	
+	return a + b;
+	
+}
+}
