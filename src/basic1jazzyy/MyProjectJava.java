72123a5379b678d3ef9e0cffc9cc94e69066b5b9
javaclass <javaclass@15000548cpu.fec.org>
2017-09-26 10:23:56 -0500

    itsjazzy

---
+++ src/basic1jazzyy/MyProjectJava.java
@@ -0,0 +1,11 @@
+package basic1jazzyy;
+
+public class MyProjectJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("frappuccino");
+
+	}
+
+}
