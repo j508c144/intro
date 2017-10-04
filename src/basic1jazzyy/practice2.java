641015178ece7a9ec8c79531c45788ccb597781f
javaclass <javaclass@15000548cpu.fec.org>
2017-10-04 13:43:42 -0500

    This is an example of a program that has a array of strings.

---
+++ src/basic1jazzyy/practice2.java
@@ -0,0 +1,19 @@
+package basic1jazzyy;
+
+public class practice2 {
+
+	public static void main(String args[]) {
+
+		String[] namesList = { "Jazzy", "JJ", "Lauren" };
+
+		for (String name : namesList) {
+			System.out.println(name.concat(" - welcome to the class"));
+		}
+
+	}
+}
+// This is an example of a program
+//that has a array of strings that list names
+//and adds welcome to the class after each. 
+
+
